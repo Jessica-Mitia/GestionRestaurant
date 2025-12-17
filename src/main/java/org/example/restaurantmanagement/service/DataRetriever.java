6fc41e4f88651827643998cd64e02c962d39ac4a
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-17 15:17:54 +0300

    add the class DataRetriever

--- src/main/java/org/example/restaurantmanagement/service/DataRetriever.java
+++ src/main/java/org/example/restaurantmanagement/service/DataRetriever.java
@@ -1,4 +1,13 @@
 package org.example.restaurantmanagement.service;
 
+import org.example.restaurantmanagement.db.DBConnection;
+import org.example.restaurantmanagement.model.Dish;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DataRetriever {
+
 }
