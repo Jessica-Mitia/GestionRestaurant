c6a5eef228a17b9ce76d4986382020a832e07bf1
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-17 15:13:37 +0300

    add the script schema.sql

---
+++ src/main/sql/schema.sql
@@ -0,0 +1,21 @@
+CREATE TYPE dish_type_enum AS ENUM ('START', 'MAIN', 'DESSERT');
+
+CREATE TABLE dish (
+    id serial
+        constraint dish_pk primary key,
+    name varchar(100) not null,
+    dish_type dish_type_enum
+);
+
+CREATE TYPE category_enum AS ENUM ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
+
+CREATE TABLE ingredient (
+    id serial
+        constraint ingredient_pk primary key,
+    name varchar(100) not null,
+    price numeric,
+    category category_enum,
+    constraint fk_dish
+        foreign key (dish_id)
+        references dish(id)
+);
