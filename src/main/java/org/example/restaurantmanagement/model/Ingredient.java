c3600cdc545b5c2f778c828b5c70c6bfa4846e48
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-17 15:16:24 +0300

    add the class Ingredient

--- src/main/java/org/example/restaurantmanagement/model/Ingredient.java
+++ src/main/java/org/example/restaurantmanagement/model/Ingredient.java
@@ -1,4 +1,86 @@
 package org.example.restaurantmanagement.model;
 
-public class DishTypeEnum {
+import java.util.Objects;
+
+public class Ingredient {
+    private int id;
+    private String name;
+    private Double price;
+    private CategoryEnum category;
+    private Dish dish;
+
+    public Ingredient(int id, String name, CategoryEnum category, Double price, Dish dish) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.dish = dish;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    public void setDish(Dish dish) {
+        this.dish = dish;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(price, that.price) && category == that.category && Objects.equals(dish, that.dish);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, category, dish);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category=" + category +
+                ", dish=" + dish +
+                '}';
+    }
+
+    public String getDishName() {
+        return dish.getName();
+    }
 }
