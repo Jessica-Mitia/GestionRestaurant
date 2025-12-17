7baa1a8e31a8882d6e0556beaf105f7141f3e259
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-17 15:17:31 +0300

    add the class DBConnection

--- src/main/java/org/example/restaurantmanagement/db/DBConnection.java
+++ src/main/java/org/example/restaurantmanagement/db/DBConnection.java
@@ -1,4 +1,19 @@
 package org.example.restaurantmanagement.db;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private final Dotenv dotenv = Dotenv.load();
+
+    private String URL = dotenv.get("JDBC_URL");
+    private String USER = dotenv.get("USER");
+    private String PASSWORD = dotenv.get("PASSWORD");
+
+    public Connection getDBConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
