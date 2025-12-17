719a6b2923a1a984ceca4ad195ec6c6382c9667d
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-17 15:18:21 +0300

    add dependencies

--- src/test/java/DishTest.java
+++ src/test/java/DishTest.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.example.restaurantmanagement.db.DBConnection;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DishTest {
+    @Test
+    void testDBConnection() throws SQLException {
+        DBConnection db = new DBConnection();
+        Connection con = db.getDBConnection();
+        Assertions.assertNotNull(con);
+        Assertions.assertFalse(con.isClosed());
+    }
 }
