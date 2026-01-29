f5641c8f6830c99a11fbe1cb968da5f4c568dc7d
Mitia Jessica <mitiah.jessica@gmail.com>
2026-01-29 08:34:42 +0300

    test the save methods

--- src/main/java/org/example/restaurantmanagement/main/Main.java
+++ src/main/java/org/example/restaurantmanagement/main/Main.java
@@ -13,52 +13,23 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws SQLException {
         DataRetriever dr = new DataRetriever();
-        Dish dish = new Dish();
-        Ingredient ingredient = new Ingredient();
-        StockValue stockValue = new StockValue();
-        StockMovement stockMovement = new StockMovement();
-
-        List<DishIngredient> dishIngredients = new ArrayList<>();
-        List<StockMovement> stockMovements = new ArrayList<>();
-
-        stockValue.setUnit(UnitTypeEnum.KG);
-        stockValue.setQuantity(4.0);
-
-        stockMovement.setId(11);
-        stockMovement.setType(MovementTypeEnum.IN);
-        stockMovement.setValue(stockValue);
-        stockMovement.setCreationDateTime(Instant.now());
-
-        stockMovements.add(stockMovement);
-
-        dish.setId(12);
-        dish.setName("Vary misy loka");
-        dish.setPrice(1800.00);
-        dish.setDishType(DishTypeEnum.MAIN);
-        dish.setDishIngredients(dishIngredients);
-
-        ingredient.setId(8);
-        ingredient.setName("Sel de mer");
-        ingredient.setPrice(300.00);
-        ingredient.setCategory(CategoryEnum.DAIRY);
-        ingredient.setStockMovementList(stockMovements);
 
         DishOrder dishOrder = new DishOrder();
-        dishOrder.setId(2);
-        dishOrder.setDish(dr.findDishById(2));
+        dishOrder.setId(21);
         dishOrder.setQuantity(2);
+        dishOrder.setDish(dr.findDishById(2));
 
         List<DishOrder> dishOrders = new ArrayList<>();
         dishOrders.add(dishOrder);
 
         Order order = new Order();
-        order.setId(2);
-        order.setReference("ORD00003");
+        order.setId(21);
+        order.setReference("ORD0002");
         order.setCreationDateTime(Instant.now());
         order.setDishOrders(dishOrders);
 
+        System.out.println(dr.saveOrder(order));
 
-        System.out.println(dr.findOrderByReference("ORD00001"));
     }
 
 }
